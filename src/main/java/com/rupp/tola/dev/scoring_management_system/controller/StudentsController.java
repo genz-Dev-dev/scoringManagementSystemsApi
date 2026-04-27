48c6cc903bfbfee344bde7d7c4b9e3bbeea49c24
tola.dev <namtola4444@gmail.com>
2026-04-28 00:34:28 +0700

    _

--- src/main/java/com/rupp/tola/dev/scoring_management_system/controller/StudentsController.java
+++ src/main/java/com/rupp/tola/dev/scoring_management_system/controller/StudentsController.java
@@ -1,23 +1,122 @@
 package com.rupp.tola.dev.scoring_management_system.controller;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
+import java.util.UUID;
 
+import com.rupp.tola.dev.scoring_management_system.dto.request.ImportStudentRequest;
+import com.rupp.tola.dev.scoring_management_system.dto.request.PaginationRequest;
+import com.rupp.tola.dev.scoring_management_system.dto.request.StudentRequest;
+import com.rupp.tola.dev.scoring_management_system.data.MultipleResponse;
+import com.rupp.tola.dev.scoring_management_system.data.SingleResponse;
+import com.rupp.tola.dev.scoring_management_system.dto.request.StudentsFilterRequest;
+import com.rupp.tola.dev.scoring_management_system.dto.response.*;
+import com.rupp.tola.dev.scoring_management_system.service.ExcelService;
+import com.rupp.tola.dev.scoring_management_system.service.StudentScoreReportService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import com.rupp.tola.dev.scoring_management_system.service.StudentService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/students")
 public class StudentsController {
 
 	private final StudentService studentService;
+	private final ExcelService excelService;
+	private final StudentScoreReportService getStudentScoreReport;
 
 	@GetMapping
+	public ResponseEntity<MultipleResponse<StudentResponse>> findAll(PaginationRequest request) {
+		Page<StudentResponse> responses = studentService.getAll(request.toPageable());
+		return ResponseEntity.ok()
+				.body(MultipleResponse.success("Successfully retrieved all students with pagination.", responses));
 	}
 
+	@GetMapping(path = "/{uuid}")
+	public ResponseEntity<SingleResponse<StudentResponse>> getById(@PathVariable UUID uuid) {
+		StudentResponse studentsResponse = studentService.getById(uuid);
+		log.info("getByUuid: {}", uuid);
+		return ResponseEntity.ok(SingleResponse.success("Successfully retrieved student.", studentsResponse));
+	}
+
+	@PostMapping
+	public ResponseEntity<SingleResponse<StudentResponse>> create(@Valid @RequestBody StudentRequest request) {
+		StudentResponse response = studentService.create(request);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(SingleResponse.success("Successfully created student.", response));
+	}
+
+	@PutMapping(path = "/{uuid}")
+	public ResponseEntity<SingleResponse<StudentResponse>> update(@PathVariable UUID uuid,
+	                                                              @Valid @RequestBody StudentRequest request) {
+		StudentResponse response = studentService.update(uuid, request);
+		return ResponseEntity.ok(SingleResponse.success("Successfully updated student.", response));
+	}
+
+	@DeleteMapping(path = "/{uuid}")
+	public ResponseEntity<SingleResponse<Void>> delete(@PathVariable UUID uuid) {
+		studentService.delete(uuid);
+		return ResponseEntity.ok(SingleResponse.success("Successfully deleted student.", null));
+	}
+
+	@GetMapping(path = "/{uuid}/classes")
+	public ResponseEntity<SingleResponse<ClassResponse>> getClassByStudentId(@PathVariable UUID uuid) {
+		ClassResponse response = studentService.getClassByStudentId(uuid);
+		return ResponseEntity.ok(SingleResponse.success("Successfully retrieved student class.", response));
+	}
+
+	@PostMapping(path = "/import-student", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SingleResponse<?>> importStudent(@ModelAttribute ImportStudentRequest request) {
+		return ResponseEntity
+				.ok(SingleResponse.success("Import student successfully.", studentService.importStudents(request)));
+
+	}
+
+	@GetMapping(path = "/export-student")
+	public ResponseEntity<Resource> exportStudent(@RequestParam UUID classId) {
+		ByteArrayInputStream stream = excelService.exportStudent(classId);
+		ByteArrayResource recourse = new ByteArrayResource(stream.readAllBytes());
+		return ResponseEntity.status(HttpStatus.OK)
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=students-" + classId + ".xlsx")
+				.contentType(MediaType.APPLICATION_OCTET_STREAM).body(recourse);
+	}
+
+	@GetMapping("/statistics")
+	public ResponseEntity<SingleResponse<StudentStatisticsResponse>> getStatistics() {
+		StudentStatisticsResponse response = studentService.statistics();
+		return ResponseEntity
+				.ok(SingleResponse.success("Successfully retrieved student statistics response.", response));
+	}
+
+	@PostMapping("/filter")
+	public ResponseEntity<SingleResponse<List<StudentsFilterResponse>>> filterStudents(
+			@RequestBody StudentsFilterRequest request) {
+
+		List<StudentsFilterResponse> response =
+				studentService.studentsFilterResponse(request);
+
+		return ResponseEntity.ok(
+				SingleResponse.success(
+						"Successfully retrieved student statistics response.",
+						response
+				)
+		);
+	}
+
+	@GetMapping("/student-scores")
+	public ResponseEntity<SingleResponse <List<StudentScoreResponse>>> getReport() {
+		return ResponseEntity.ok(SingleResponse
+				.success("Successfully retrieved student statistics response.", getStudentScoreReport.getStudentScoreReport()));
+
+	}
 }
