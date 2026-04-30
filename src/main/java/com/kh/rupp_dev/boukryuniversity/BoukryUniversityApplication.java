f066c6452bd07b88bb900bcd17a96350ca256e9f
ChamreunVira <virachamreun@gmail.com>
2026-04-30 08:52:01 +0700

    feat added redis for data caching in memory improved performace

--- src/main/java/com/kh/rupp_dev/boukryuniversity/BoukryUniversityApplication.java
+++ src/main/java/com/kh/rupp_dev/boukryuniversity/BoukryUniversityApplication.java
@@ -2,10 +2,11 @@ package com.kh.rupp_dev.boukryuniversity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
+@EnableCaching
 public class BoukryUniversityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoukryUniversityApplication.class, args);
